2efb95c51e4d4723be77b4a4d15d64826732b7e1
Alasdair Vincent <ajvincent@aapt.net.au>
2020-04-23 00:33:46 +0800

    Added trivia to-do

--- commands/fun/trivia.js
+++ commands/fun/trivia.js
@@ -1,7 +1,22 @@
+//Short term TO-DO
+//Read trivia.json
+//Read preset questions from trivia.json and send them to discord
+//Listen for correct answer
+//message.channel.send(`Congratulations! ${member.user.displayName} got it!`); when correct answer is given
+
+
+const fs = require('fs');
+
 module.exports = {
     name: 'trivia',
     category: 'fun',
     run: (client, message, args) => {
-        message.channel.send("WORK GOD DAMMIT!");
+        
+        var data = fs.readdirSync();
+        var length = data.trivia.length;
+        var randomNumber = Math.floor(Math.random * length);
+
+        console.log(randomNumber);
+
     }
 }
